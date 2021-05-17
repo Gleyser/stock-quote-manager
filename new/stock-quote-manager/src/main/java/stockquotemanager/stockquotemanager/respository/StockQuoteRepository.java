5601e93d6758f1342b0a64303fb33dc2cff3e8cb
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2021-05-17 15:14:33 -0300

    organized files

--- new/stock-quote-manager/src/main/java/stockquotemanager/stockquotemanager/respository/StockQuoteRepository.java
+++
@@ -1,9 +0,0 @@
-package stockquotemanager.stockquotemanager.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import stockquotemanager.stockquotemanager.model.StockQuote;
-
-public interface StockQuoteRepository extends JpaRepository<StockQuote, Long> {
-
-}
